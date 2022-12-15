c3c4ce2b84e01179ccc68b014fb779ad2a5bd8ac
Manisha Kumari Gupta <64262752+manishaKgupta@users.noreply.github.com>
2022-12-15 16:32:31 +0530

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-sudo apt update -y 
-sudo apt install nginx -y
-sudo systemctl enable nginx
-sudo systemctl start nginx
-sudo apt install php php-fpm php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip -y
+# updating the linux
+apt update && apt upgrade -y
