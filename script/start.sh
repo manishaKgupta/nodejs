c0e2afbf77e50568ff1600dcb61ff8e4a63f4d50
Manisha Kumari Gupta <64262752+manishaKgupta@users.noreply.github.com>
2022-12-15 16:43:26 +0530

    Update start.sh

--- script/start.sh
+++ script/start.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-sudo systemctl start nginx
+# Changing required permissions
+sudo chmod -R 777 /home/ubuntu/nodejs
+cd /home/ubuntu/nodejs
+# Starting the nodejs application using pm2 service
+pm2 start app.js
